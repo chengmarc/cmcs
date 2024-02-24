49326f85a3bd66baa64e216aecdc0662e1297c04
chengmarc <last@live.ca>
2024-02-24 11:15:11 -0500

    enabled console in executable

--- core-process/cmcs_distribute.bat
+++ core-process/cmcs_distribute.bat
@@ -2,5 +2,5 @@ pyinstaller ^
 	--distpath .\distribution ^
 	--workpath .\distribution ^
 	--name CMCScan --add-data "cmcs_icon.ico;." --icon=cmcs_icon.ico ^
-	--onefile --noconsole cmcs_gui.py
+	--onefile cmcs_gui.py
 pause
