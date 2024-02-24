48cbb0f0e60e7dc3e0e374a17c80c2232ae268ac
chengmarc <last@live.ca>
2024-02-24 11:43:39 -0500

    added module exclusion

--- core-process/cmcs_distribute.bat
+++ core-process/cmcs_distribute.bat
@@ -1,6 +1,18 @@
 pyinstaller ^
 	--distpath .\distribution ^
 	--workpath .\distribution ^
+	--exclude-module matplotlib ^
+    --exclude-module scipy ^
+    --exclude-module numpy ^
+    --exclude-module setuptools ^
+    --exclude-module hook ^
+    --exclude-module distutils ^
+    --exclude-module hooks ^
+    --exclude-module PIL ^
+    --exclude-module PyQt4 ^
+    --exclude-module PyQt5 ^
+    --exclude-module sqlite3 ^
+    --exclude-module beautifulsoup4 ^
 	--name CMCScan --add-data "cmcs_icon.ico;." --icon=cmcs_icon.ico ^
 	--onefile cmcs_gui.py
 pause
